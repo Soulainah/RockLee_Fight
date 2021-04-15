eee9c02a874581437229ccef45de1a3379313e39
Soulainah <Soulainah>
2021-04-15 09:22:33 +0200

    push des mise à jour

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //declare les variable des images
 function attaque() {
-    var rocklee = document.querySelector('.rocklee');
     var rockleeattaque = document.querySelector('.rocklee').src='/home/descodeuses/Documents/Projets/Rocklee_Fight/images/rocklee/attaque.png';
  
 }
